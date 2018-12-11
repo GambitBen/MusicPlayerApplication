c1862fcaf633e26035b871a86c61c715b6336317
GambitBen <yorai.eran@gmail.com>
2018-12-11 16:16:36 +0200

    viewPager update

--- app/src/main/java/com/musicplayer/yorai/musicplayerapplication/MainActivity/MainActivity.java
+++ app/src/main/java/com/musicplayer/yorai/musicplayerapplication/MainActivity/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.PorterDuff;
 import android.net.Uri;
 import android.os.Build;
 import android.os.IBinder;
@@ -36,6 +37,7 @@ import com.musicplayer.yorai.musicplayerapplication.Logic.MusicService;
 import com.musicplayer.yorai.musicplayerapplication.R;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {//implements MediaPlayerControl {
 
@@ -53,6 +55,7 @@ public class MainActivity extends AppCompatActivity {//implements MediaPlayerCon
      * The {@link ViewPager} that will host the section contents.
      */
     private ViewPager mViewPager;
+    private TabLayout tabLayout;
 
     public static ArrayList<Song> currentPlaylist;
     public static ArrayList<Song> songDatabase;
@@ -103,22 +106,43 @@ public class MainActivity extends AppCompatActivity {//implements MediaPlayerCon
             songDatabase = new ArrayList<Song>();
             getSongList();
         }
+        initComponent();
+        actionHandle();
 
+    }
+
+    private void initComponent() {
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-        // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.view_pager);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        setupViewPager(mViewPager);
         tabLayout.setupWithViewPager(mViewPager);
 
         currentPlaylist = new ArrayList<Song>();
 
-        //setController();
+        tabLayout.getTabAt(0).setText("Songs");
+        tabLayout.getTabAt(1).setText("Artists");
+        tabLayout.getTabAt(2).setText("Albums");
+    }
+
+    private void actionHandle() {
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
     }
 
     public void getSongList() {
@@ -336,6 +360,14 @@ public class MainActivity extends AppCompatActivity {//implements MediaPlayerCon
     ///////////////////////////////////////ACTION BAR AND TABS//////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////////////////////////////
 
+    private void setupViewPager(ViewPager viewPager) {
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        mSectionsPagerAdapter.addFragment(AllSongsFragmentTab.newInstance(1), "Songs");
+        mSectionsPagerAdapter.addFragment(AllSongsFragmentTab.newInstance(2), "Albums");
+        mSectionsPagerAdapter.addFragment(AllSongsFragmentTab.newInstance(3), "Artists");
+        viewPager.setAdapter(mSectionsPagerAdapter);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -359,91 +391,41 @@ public class MainActivity extends AppCompatActivity {//implements MediaPlayerCon
     }
 
     /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
-
-    /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
      * one of the sections/tabs/pages.
      */
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public SectionsPagerAdapter(FragmentManager manager) {
+            super(manager);
         }
 
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            switch (position) {
-                case 0:
-                    return AllSongsFragmentTab.newInstance(1);
-                case 1:
-                    return AllSongsFragmentTab.newInstance(2);
-                case 2:
-                    //return ArtistsFragmentTab.newInstance(3);
-                    return AllSongsFragmentTab.newInstance(3);
-                case 3:
-                    return AllSongsFragmentTab.newInstance(4);
-                case 4:
-                    return AllSongsFragmentTab.newInstance(5);
-            }
-            return AllSongsFragmentTab.newInstance(position + 1);
+            return mFragmentList.get(position);
         }
 
         @Override
         public int getCount() {
-            // Show 5 total pages.
-            return 5;
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        public String getTitle(int position) {
+            return mFragmentTitleList.get(position);
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "Favorites";
-                case 1:
-                    return "Playlists";
-                case 2:
-                    return "Artists";
-                case 3:
-                    return "Albums";
-                case 4:
-                    return "All Songs";
-            }
             return null;
         }
     }
+
 }
